9e61efe73c63c9fd1b8bcab993c5af3f85ef16b2
lcooldong <n_cooldong@naver.com>
2021-11-24 12:21:25 +0900

    .

--- QT/WiFi_Dashboard/main.py
+++ QT/WiFi_Dashboard/main.py
@@ -13,10 +13,10 @@ class ESP12S(QDialog):
         self.widget1 = GaugeWidget(parent=self.widget1)  # parent 에 자기 자신 사이즈 정보 전달
 
         # 그래프 설정, 플롯 생성
-        self.graph1.setBackground('w')
-        self.graph1.setYRange(0, 300)
-        self.graph1.showGrid(x=True, y=True)
-        self.plot1 = self.graph1.plot(pen='k')
+        # self.graph1.setBackground('w')
+        # self.graph1.setYRange(0, 300)
+        # self.graph1.showGrid(x=True, y=True)
+        # self.plot1 = self.graph1.plot(pen='k')
 
 
 if __name__ == '__main__':
